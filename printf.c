6dd630afb60281a7867824a4d28f22627659a2d7
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2023-09-17 13:44:42 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -3,6 +3,47 @@
 #include <stdarg.h>
 #include "main.h"
 /**
+ * _putchar - ...
+ * @c: ...
+ * description: ...
+ * Return: ...
+*/
+void _putchar(char c)
+{
+	write(1, &c, 1);
+}
+/**
+ * _puts - ...
+ * @str: ...
+ * description: ...
+ * Return: ...
+*/
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str)
+	{
+	while (str[i] != '\0')
+	{
+	_putchar(str[i]);
+	i++;
+	}
+	}
+}
+/**
+ * _percent - ...
+ * description: ...
+ * Return: ...
+*/
+void _percent(void)
+{
+	char ch = '%';
+
+	_putchar(ch);
+}
+/**
  * _printf - print the string
  * @format: a character string
  * description: declare the value
