79f350a779783a7d5997fe8eda331ba74a98bd6b
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-17 23:11:12 -0700

    printf

--- print_int.c
+++ print_int.c
@@ -6,13 +6,12 @@
 */
 int _printf(const char *format, ...)
 {
-	int digit = 0;
 	int digit_count = 0;
-	int div = 1;
 	va_list args;
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(args, format);
 	while (*format)
 	{
@@ -23,45 +22,46 @@ int _printf(const char *format, ...)
 			{
 				int number = va_arg(args, int);
 				int tmp = number;
-				number == 0 ? (write(1, "0", 1), digit_count++) : 0;
-				number < 0 ? (write(1, "-", 1), digit_count++, number = -number) : 0;
+				int digit = 0;
+				int div = 1;
+
+				number == 0 ? (putchar('0'), digit_count++) : 0;
+				number < 0 ? (putchar('-'), digit_count++, number = -number) : 0;
+				if (tmp != 0)
 				{
-					if (tmp != 0)
+					while (tmp != 0)
 					{
-						while (tmp != 0)
-						{
-							tmp /= 10;
-							digit++;
-						}
-						for (int i = 1; i < digit; i++)
-						{
-							div = div * 10;
-						}
-						while (div != 0)
-						{
-							int digit = number / div;
-							char num_char = digit + '0';
-							write(1, &num_char, 1);
-							digit_count++;
-							number %= div;
-							div = div / 10;
-						}
+						tmp /= 10;
+						digit++;
+					}
+					for (int i = 1; i < digit; i++)
+					{
+						div = div * 10;
+					}
+					while (div != 0)
+					{
+						int digit = number / div;
+						char num_char = digit + '0';
+						putchar(num_char);
+						digit_count++;
+						number %= div;
+						div = div / 10;
 					}
 				}
 			}
 			else if (*format == '%')
-				{
-					write(1, "%", 1);
-					digit_count++;
-				}
-			}
-			else
 			{
-				write(1, format, 1);
+				putchar('%');
 				digit_count++;
 			}
-			format++;
 		}
-		va_end(args);
-		return (digit_count);
+		else
+		{
+			putchar(*format);
+			digit_count++;
+		}
+		format++;
 	}
+	va_end(args);
+	return (digit_count);
+}
