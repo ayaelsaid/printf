e9fd29b95fc8ccfe55b064647e82149af1f0e2b5
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-18 17:00:16 -0700

    printf

--- main.h
+++ main.h
@@ -5,10 +5,27 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+
+/**
+ * struct specifiers - def specifier type for print
+ * * @specifier:input type
+ * @print: function to print
+ */
+typedef struct specifiers
+{
+char *specifier;
+int (*print)(va_list);
+} specifiers;
+
+
+
 int _putchar(char c);
 int _puts(const char *str);
 int _percent(void);
 int _printf(const char *format, ...);
 int fp_intger(int n);
+int print_int(va_list arg);
+
+
 
 #endif
