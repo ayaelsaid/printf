d1a034727e04be42aef01b6c78df0a34eccbeec3
ayaelsaid <ayae7675@gmail.com>
2023-09-19 22:52:05 -0700

    int_print.c

--- main.h
+++ main.h
@@ -21,6 +21,10 @@ int sp_puts(va_list arg);
 int sp_print_char(va_list arg);
 int sp_print_int(va_list arg);
 int sp_print_binary(va_list arg);
+int sp_print_hex_uc(va_list arg);
+int sp_print_hex_lc(va_list arg);
+int sp_print_octal(va_list arg);
+int sp_print_unint(va_list arg);
 int (*get_sp_func(char s))(va_list arg);
 int _printf(const char *format, ...);
 
---
+++ u_print.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * sp_print_unint - ...
+ * @arg: ...
+ * Return: Number of digits printed
+ */
+int sp_print_unint(va_list arg)
+{
+	int number = va_arg(arg, int);
+	unsigned int div = 1;
+	int num_count = 0;
+
+	if (number < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (; number / div > 9; div *= 10)
+			;
+		for (; div >= 1; div /= 10)
+		{
+			int output = number / div;
+
+			_putchar('0' + output);
+			number %= div;
+			num_count++;
+		}
+	}
+	return (num_count);
+}
