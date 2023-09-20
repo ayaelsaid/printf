675978406c0858c50a5e936cfa880b55e437ea95
ayaelsaid <ayae7675@gmail.com>
2023-09-19 22:35:31 -0700

    hex_print.c

---
+++ hex_print.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * sp_print_hex_lc - ...
+ * @arg: ...
+ * Return: ...
+*/
+
+int sp_print_hex_lc(va_list arg)
+{
+	int number = va_arg(arg, int);
+	int i = 0;
+	int j;
+	int number_count = 0;
+	int hex_num[64];
+	int reminder = 0;
+
+	if (number == 0)
+	{
+		_putchar('0');
+		number_count += 1;
+		return (number_count);
+	}
+	while (number > 0)
+	{
+		reminder = number % 16;
+		if (reminder < 10)
+		{
+			hex_num[i] = reminder + '0';
+		}
+		else
+		{
+			hex_num[i] = reminder - 10 + 'a';
+		}
+		number /= 16;
+		i++;
+		number_count++;
+	}
+	j = i - 1;
+	while (j >= 0)
+	{
+		_putchar(hex_num[j]);
+		j--;
+	}
+	return (number_count);
+}
